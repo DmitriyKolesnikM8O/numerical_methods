22a32b955903aeaacb22d95df21fe5de82d9b6cf
Dmitriy <kolesnik982011@gmail.com>
2025-10-19 20:54:05 +0300

    add graphic for 2.1

---
+++ tasks/2/2.1/plot_function.py
@@ -0,0 +1,14 @@
+# Это твой "черновик", чтобы просто найти корень глазами
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+    return np.log(x + 1) - 2 * x + 0.5
+
+x_vals = np.linspace(-0.5, 2, 400) # Широкий диапазон
+y_vals = f(x_vals)
+
+plt.plot(x_vals, y_vals)
+plt.grid(True)
+plt.axhline(0, color='black')
+plt.show()
